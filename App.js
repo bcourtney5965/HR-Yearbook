60357f8097010664c5825dbfafad3b2e59822d3a
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-04-16 20:36:05 -0700

    Initialized project

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Hello, World</h1>
+        <b>Bold</b>
+      </div>
+    );
+  }
+}
+
+// const App = () => <h1>Hello Eggheads</h1> class-less compnenet
+
+export default App
